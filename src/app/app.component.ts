f671991ba024f889a37089c871d2cde6b8881679
nayfin <natjkern@gmail.com>
2017-09-01 08:36:26 -0500

    basic firebase initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { initializeApp, database} from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Seed Title';
+  constructor() {
+
+    const config = {
+      apiKey: 'AIzaSyDB10dQMqsZg2WDyg6gQ2MSeRRME71oDHQ',
+      authDomain: 'fir-lessons-aabef.firebaseapp.com',
+      databaseURL: 'https://fir-lessons-aabef.firebaseio.com',
+      projectId: 'fir-lessons-aabef',
+      storageBucket: 'fir-lessons-aabef.appspot.com',
+      messagingSenderId: '847664519994'
+    };
+    initializeApp(config);
+
+    const root = firebase.database().ref();
+    root.on('value', (snap)=>{
+      console.log(snap);
+    });
+  }
 }
