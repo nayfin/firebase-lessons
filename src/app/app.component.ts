09542d1661721f4fe6541cd0354973952b4509d9
nayfin <natjkern@gmail.com>
2017-09-02 23:46:43 -0500

    set up buttons and played with angularfire pushing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +7,34 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Seed Title';
+  title = 'Firebase Sandbox';
+
+  courses$: FirebaseListObservable<any>;
+  firstCourse$: FirebaseObjectObservable<any>;
   constructor(private db: AngularFireDatabase) {
 
-    const courses$: FirebaseListObservable<any> = db.list('courses');
+    this.courses$ = db.list('courses');
+    this.courses$.subscribe(console.log);
 
-    courses$.subscribe(
-      val => console.log('courses', val)
-    );
+    this.firstCourse$ = db.object('courses/-Kt2Go8gofwGeNMb1f7p');
+    this.firstCourse$.subscribe(console.log);
+  }
+
+  listPush() {
+    this.courses$.push({description: 'Test list push'});
+  }
+
+  listRemove() {
+
+  }
+
+  listUpdate() {
+
+  }
+  objSet() {
+
+  }
+  objUpdate() {
+
   }
 }
